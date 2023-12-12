53796f3da39a6f0a957a76825574d7cd17521be3
Mariel Duran <marielduran84@gmail.com>
2023-12-12 11:34:28 +0200

    Update

--- day3.py
+++ day3.py
@@ -1,7 +1,7 @@
 import string
 
 
-with open("sample.txt") as file:
+with open("sample_day3.txt") as file:
     
     data = file.readlines()
     part_numbers = []
@@ -14,12 +14,12 @@ with open("sample.txt") as file:
         
         nums = [x for x in numbers if x != "" and x not in string.punctuation]
         
-        
-        print(nums)
 
         if len(nums) == 0: # ignore empty lists
             line_position += 1
             continue
+
+        print(nums)
 
         for num in nums:
             num_position = line.find(num)
@@ -30,12 +30,16 @@ with open("sample.txt") as file:
                 line_range = [x for x in range(num_position -1, num_position + (num_length + 1)) if x <= line_length and x >= 0]
             print(num)
             # first scenario: number has special character next to it in the same line - either before or after num
+            
             if any(x in string.punctuation for x in num):
+                print("First scenario run")
                 for spec_char in string.punctuation:
                     if spec_char in num:
                         try:
                             holder = [x for x in num.split(spec_char) if x != ""]
                             print(holder)
+                            if len(holder) <= 1:
+                                raise 
                             part_numbers.append(int(holder[0].strip(string.punctuation)))
                             part_numbers.append(int(holder[1].strip(string.punctuation)))
                             print("Try completed")
@@ -46,25 +50,29 @@ with open("sample.txt") as file:
                             break
             # second scenario: number has special character adjacent in the previous line or next line
             # previous line
-            elif line_position > 0:
-                for spec_char in string.punctuation:
-                    if spec_char in data[line_position - 1]:
-                        if data[line_position - 1].find(spec_char) in line_range:
-                            part_numbers.append(int(num))
-                            break
-                        else:
-                            continue
+            else:
+                if line_position > 0:
+                    print("Previous line scenario run")
+                    for spec_char in string.punctuation:
+                        if spec_char in data[line_position - 1]:
+                            if data[line_position - 1].find(spec_char) in line_range:
+                                part_numbers.append(int(num))
+                                break
+                            else:
+                                continue
 
-            # next line
-            elif line_position < len(data) - 1:
-                for spec_char in string.punctuation:
-                    if spec_char in data[line_position + 1]:
-                        if data[line_position + 1].find(spec_char) in line_range:
-                            part_numbers.append(int(num))
-                            break
-                        else:
-                            continue
+                # next line
+                if line_position < len(data) - 1:
+                    print("Next line scenario run")
+                    for spec_char in string.punctuation:
+                        if spec_char in data[line_position + 1]:
+                            if data[line_position + 1].find(spec_char) in line_range:
+                                part_numbers.append(int(num))
+                                break
+                            else:
+                                continue
 
         line_position += 1
+    print(part_numbers)
     print(sum(part_numbers))
 
---
+++ day4.py
@@ -0,0 +1,29 @@
+with open("sample.txt") as file:
+    
+    data = file.readlines()
+    points = 0
+
+    for line in data:
+        # winning_nums = line.split("|")[0].split(":")[1].split()
+        winning_nums = [int(x) for x in line.split("|")[0].split(":")[1].split()]
+        given_nums = [int(x) for x in line.split("|")[1].split()]
+        print(winning_nums)
+        print(given_nums)
+        points_holder = 0
+
+        # ignore non matches
+
+        if not any(x in winning_nums for x in given_nums):
+            continue
+
+        for win_num in winning_nums:
+            for giv_num in given_nums:
+                if win_num == giv_num:
+                    if points_holder < 1:
+                        points_holder+= 1
+                    else:
+                        points_holder = points_holder*2
+        
+        points = points + points_holder
+
+    print(points)
